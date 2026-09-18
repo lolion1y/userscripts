4a2a99e52dfb555694f9e7ad86ed9736b8738cb9
lolion1y <93830081+lolion1y@users.noreply.github.com>
2026-09-18 13:14:44 +0800

    bump version(dmhy/treeview): 0.35.4

--- dmhy/treeview.user.js
+++ dmhy/treeview.user.js
@@ -2,7 +2,7 @@
 // @name         dmhy tree view
 // @namespace    https://greasyfork.org/zh-CN/scripts/26430-dmhy-tree-view
 // @license      GPL version 3
-// @version      0.35.3
+// @version      0.35.4
 // @description  convert plain file list into a tree view for 动漫花园 (share.dmhy.org)
 // @author       lolion1y
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
